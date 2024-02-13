da74fac3244118f634fe1540a3156880877ef897
Ayushsinghpanwar99 <135314352+Ayushsinghpanwar99@users.noreply.github.com>
2024-02-13 13:30:08 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import './App.css';
+const Home = () => {
+    return (
+        <>
+            <div class="headere">
+                <div className='A1'><img className='A2' src="https://www.companieslogo.com/img/orig/ABNB_BIG-9ccc2025.png?t=1633511992" height="30px" width="130px" /> </div>
+                <div className="D1">
+                    <ul className='A3'>
+                        <li><a href="#">Stays</a></li>
+                        <li><a href="#">Experiences</a></li>
+                        <li><a href="#">Online Experiences</a></li>
+                    </ul>
+                </div>
+                <div className='A4'><img className="A5" src="https://framerusercontent.com/images/RkV86hKSZ17yNsuZKWylbt86ZiI.png" height="40px" width="40px" align="right" /> </div>
+            </div>
+            <hr />
+            <div class="headere1">
+                <div className='A6'><button className='A7'><img src="n.jpeg" height="48px" /></button></div>
+                <div className='A8'>
+                    <div>
+                        <button className='A9'>Anywhere<img className='A0' src='https://cdn-icons-png.flaticon.com/128/2985/2985150.png'height="18px" width="18px"/></button>
+                    </div>
+                    {/* <select className='A9'>
+                        <option value="volvo">  <pre>Anywhere</pre></option>
+                        <option value="saab">Indore</option>
+                        <option value="opel">Ujjain</option>
+                        <option value="audi">Bhopal</option>
+                    </select> */}
+                    <div>
+                        <button className='A10'><img className='A11' src="https://cdn-icons-png.flaticon.com/128/2948/2948088.png" height="15px" width="15px" />June 14 - 21</button></div>
+                    <div className='A12'><button className='A13'>+</button>  4 guests  <button className='A14'>-</button> </div>
+                </div>
+                <div className='A15'><button className='A16'><img src="https://img.icons8.com/?size=1x&id=ZHhzJTWCl4eP&format=png" height="30px" />
+
+                </button> </div>
+            </div>
+
+            <hr />
+            <div className='A17'>
+                <nav>
+                    <lu>
+                        <li className='A18'><img src="https://img.icons8.com/?size=1x&id=AO47dbOTMV5N&format=png" height="30px" width="30px" /> <p className='A19'>All homes</p> </li>
+                        <li className='A20' ><img src="https://img.icons8.com/?size=1x&id=wxbxh8Nve9IX&format=png" height="30px" width="30px" /><p className='A21'>Amazing views</p></li>
+                        <li className='A22'><img src="https://img.icons8.com/?size=512&id=49608&format=png" height="30px" width="30px" /><p className='A23'>Tiny Homes</p></li>
+                        <li className='A24'><img src="https://img.icons8.com/?size=1x&id=5388&format=png" height="30px" width="30px" /><p className='A25'>Chefs kitchen</p></li>
+                        <li className='A26'><img src="https://cdn-icons-png.flaticon.com/128/3562/3562699.png" height="30px" width="30px" /><p className='A27'>Surfing</p></li>
+                        <li className='A28'><img src="https://cdn-icons-png.flaticon.com/128/887/887345.png" height="30px" width="30px" /><p className='A29'>Mansions</p></li>
+                        <li className='A30'><img src="https://cdn-icons-png.flaticon.com/128/2953/2953328.png" height="30px" width="30px" /><p className='A31'>Luxe</p></li>
+                        <li className='A32'><img src="https://cdn-icons-png.flaticon.com/128/1054/1054194.png" height="30px" width="30px" /><p className='A33'>TreeHouse</p></li>
+                        <li className='A34'><img src="https://t3.ftcdn.net/jpg/04/55/64/50/240_F_455645084_PXFgQxr3nyP27UlqH6bZtoIL7trjS7V4.jpg" height="30px" width="30px" /> <p className='A35'>Comping</p></li>
+                        <li className='A36'><img src="https://cdn-icons-png.flaticon.com/128/3319/3319235.png" height="30px" width="30px" /> <p className='A37'>Beach front</p></li>
+                        <li className='A38'><img src="https://cdn-icons-png.flaticon.com/128/2919/2919302.png" height="30px" width="30px" /><p className='A39'>Fram</p></li>
+                        <li className='A40'><img src="https://cdn-icons-png.flaticon.com/128/2943/2943887.png" height="30px" width="30px" /><p className='A41'>Castles</p></li>
+                        <li className='A42'><img src="https://cdn-icons-png.flaticon.com/128/2943/2943887.png" height="30px" width="30px" /><p className='A43'>Islands</p></li>
+                        <li className='A44'><img src="https://cdn-icons-png.flaticon.com/128/7417/7417554.png" height="30px" width="30px" /> <p className='A45'>Amazing Pools</p></li>
+                        <li className='A46'><img src="  https://cdn-icons-png.flaticon.com/128/3437/3437732.png" height="30px" width="30px" /><p className='A47'>Skiing</p></li>
+                        <li className='A48'><img src="https://cdn-icons-png.flaticon.com/128/881/881245.png" height="30px" width="30px" /><p className='A49'>Desert</p></li>
+                    </lu>
+                </nav>
+            </div>
+            <div className='A50'>
+                <div id="d1" className='A51'><img src="https://framerusercontent.com/images/TQv9Req7ZME40dmudtRtkwz9lcQ.webp" height="216px" className='A52' /><h2 id="h1">  whitefish Estate </h2><p id="i1">whitefish,Montana,Unites States</p><h3 class="r1" className='A53'>$200000/night</h3></div>
+                <div id="d1" className='A54'><img src="https://framerusercontent.com/images/afIWyPD7bAqXpOTuGYeO4FzrnU.webp" height="216px" className='A55' /><h2 id="h1">  Luxuxry stay insaint </h2><p id="i1">weston,saint james,Unites States</p><br /><h3 class="r1">$820000/night</h3></div>
+                <div id="d1" className='A56'><img src="https://th.bing.com/th/id/OIP.aKdZeOjJ_Au2gNmjD-hAuQHaE8?pid=ImgDet&w=206&h=137&c=7&dpr=1.3" height="216px" className='A57' /><h2 id="h1">  Luxuxry stay in weston</h2><br /><p id="i1">whitefish,Montana,Unites States</p><h3 class="r1" className='A58'>$100/night</h3></div>
+                <div id="d1" className='A59'><img src="https://th.bing.com/th/id/OIP.fnc_3Tw-cirZLFblWTDgAgHaE8?pid=ImgDet&w=206&h=137&c=7&dpr=1.3" height="216px" className='A60' /><h2 id="h1">  whitefish Estate </h2><br /><p id="i1">weston,saint james,Unites States</p><br /><h3 class="r1">$400000000/night</h3></div>
+                <div id="d1" className='A61'><img src="https://th.bing.com/th/id/OIP.0w7OeJ7w3oZ0k8Lo11aYIwHaE8?w=306&h=204&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='A62' /><h2 id="h1">  whitefish Estate </h2><br /><p id="i1">whitefish,Montana,Unites States</p><br /><h3 class="r1">$1000000000/night</h3></div>
+                <div id="d1" className='A63'><img src="https://th.bing.com/th/id/OIP.nW5KEq26P5eMfmih5fz-RgHaEK?w=283&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="206px" className='A64' /><h2 id="h1">  Bohemain & Spacious/Private </h2><br /><p id="i1">Tulum,Quinatan,Unites States</p><br /><h3 class="r1">$100/night</h3></div>
+                <div id="d1" className='A65'><img src="https://th.bing.com/th/id/OIP.8r7i-GO_JcvqNnYcBTSwNgHaEK?w=292&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="200px" className='A66' /><h2 id="h1">  whitefish Estate </h2><br /><p id="i1">whitefish,Montana,Unites States</p><br /><h3 class="r1">$10,000/night</h3></div>
+                <div id="d1" className='A67'><img src="https://th.bing.com/th/id/OIP.gOGFgO8J707GndretmXuAgHaEK?w=324&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7" width="330p" height="216px" className='A68' /><h2 id="h1">  Bohemain & Spacious/Private </h2><br /><p id="i1">Tulum,Quinatan,Unites States</p><br /><h3 class="r1">$10,0000000890/night</h3></div>
+                <div id="d1" className='A69'><img src="https://th.bing.com/th/id/OIP.9W4vmpZzbgb4d6OQs4J4fgHaE8?w=272&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='A70' /><h2 id="h1">  7,500 sq ft Private Beachfront </h2><br /><p id="i1">Dubai,Unites Arab Emitras</p><br /><h3 class="r1">$10,000009000/night</h3></div>
+                <div id="d1" className='A71'><img src="https://th.bing.com/th/id/OIP.wOBwEMK0gg3phkpN09iBOwHaE7?w=265&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='A72' /><h2 id="h1">  whitefish Estate </h2><br /><p id="i1">whitefish,Montana,Unites States</p><br /><h3 class="r1">$10,000000000/night</h3></div>
+                <div id="d1" className='A73'><img src="https://th.bing.com/th/id/OIP.SK-FQhlZxr8rIpjJHEs2ewHaE8?w=268&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='A74' /><h2 id="h1">  Numero22-Lake como-DEsign </h2><br /><p id="i1">lagalio,Lombardai,Itlay</p><br /><h3 class="r1">$10,000000000/night</h3></div>
+                <div id="d1" className='A75'><img src="https://images.unsplash.com/photo-1591825729269-caeb344f6df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWlyYm5ifGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" height="216px" className='A76' /><h2 id="h1">  Luxuxry stay in weston,saint </h2><br /><p id="i1"> Luxuxry stay in weston,saint james</p><br /><h3 class="r1">$10,000/night</h3></div>
+            </div>
+        </>
+    )
+}
+
+export default Home
